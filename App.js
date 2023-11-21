94fc670b6ec46b27a09390bc221691fd911dcb60
Félix Perroud <felix@Felixs-MacBook-Air.local>
2023-11-21 10:04:55 +0100

    init

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import Animated, {
+  useSharedValue,
+  SlideInLeft,
+  withTiming,
+  SlideInDown,
+  reduceMotion,
+  ReduceMotion,
+} from "react-native-reanimated";
 
 export default function App() {
+  const width = useSharedValue(100);
+
+  const handlePress = () => {
+    (width.value = withTiming(width.value + 50)),
+      () => {
+        console.log("done");
+      };
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Animated.View
+        style={styles.box}
+        entering={SlideInDown.delay(500).withCallback((finished) => {
+          console.log(`finished without interruptions: ${finished}`);
+        })}
+      >
+        <Text>Hello</Text>
+      </Animated.View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +38,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    backgroundColor: "blue",
+    height: 100,
+    width: 100,
   },
 });
